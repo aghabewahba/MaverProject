986ea77c58cb9497f350bc16070f5a42003b668b
aghabewahba <aghaby69@hotmail.com>
2021-05-23 19:28:49 -0400

    more work

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println(" hello sdet");
+        System.out.println("git hub is already published to github");
     }
 }
